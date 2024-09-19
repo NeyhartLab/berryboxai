69f8d69120fbe769ff775306649b50d437dcac15
neyhartj <neyhartje@gmail.com>
2024-09-19 10:00:20 -0400

    setup edits to berrybox

--- berryboxai/main.py
+++ berryboxai/main.py
@@ -254,7 +254,7 @@ def main():
         model_name = "berrybox_" + mod + ".pt"
 
     # This finds the model within the package structure
-    package_dir = pkg_resources.resource_filename('berryboxai, '')
+    package_dir = pkg_resources.resource_filename('berryboxai')
     model_path = os.path.join(package_dir, 'data', 'weights', model_name)
 
     model = YOLO(model_path, task = task)
